91b53c3d791aac1b02b1c81c4384ffc75d207e01
Adriano <adrianosouzaa733@gmail.com>
2025-04-13 14:21:22 -0300

    tratamento de rota não existente

---
+++ client/src/app/home/index.tsx
@@ -0,0 +1,8 @@
+
+export default function HomePage(){
+  return(
+    <main className="bg-white h-screen">
+      <h1 className="text-black">Funcionando pagina home</h1>
+    </main>
+  )
+}
---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function NotFound(){
+  return(
+    <main className="bg-white h-screen flex justify-center items-center flex-col space-y-4">
+      <h1 className="text-black flex justify-center items-center text-4xl">Ops, essa pagina não existe</h1>
+      <h2 className="text-red-500 text-3xl">404</h2>
+      <hr />
+      <Link href="/" className="text-black text-2xl">Home</Link>
+    </main>
+  )
+}
