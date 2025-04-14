28a86f4a37b187703e44a30ce1371021cceeff9f
julindoti <julinmestredoti@gmail.com>
2025-04-14 09:15:01 -0300

    t

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,6 @@
 import express from 'express';
 
 import userRoutes from './routes/user';
-<<<<<<< HEAD
-=======
-import router from './routes/user';
->>>>>>> f1a6b7c06a1772cfe3545f331e33a9d871011ddc
 
 const app= express();
 const PORT = 3333;
@@ -15,11 +11,6 @@ app.use('/api/users', userRoutes);
 app.listen(3333, ()=>{
 
     console.log("No ar e rodando")
-<<<<<<< HEAD
 
 })
-=======
-})
 
-export default router();
->>>>>>> f1a6b7c06a1772cfe3545f331e33a9d871011ddc
