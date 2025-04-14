fa7287d0ff58fac239b7776109dd23c259424ce5
julindoti <julinmestredoti@gmail.com>
2025-04-13 21:03:00 -0300

    envio

---
+++ client/src/app/components/header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+ 
+export default function Header(){
+
+return(
+
+    <div className="bg-blue-950 min-w-screen h-16  fixed justify-start p-4 text-left"> 
+
+         <h1>Alboom </h1>
+    </div>
+);
+
+
+
+}
+
