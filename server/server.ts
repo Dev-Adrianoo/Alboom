bb1baa4aaa1a708efbd3f9c1ec7ea54041b38a53
julindoti <julinmestredoti@gmail.com>
2025-04-14 08:40:14 -0300

    .txt

---
+++ server/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import userRoutes from './routes/user';
+
+const app= express();
+const PORT = 3333;
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+
+app.listen(3333, ()=>{
+
+    console.log("No ar e rodando")
+
+})
