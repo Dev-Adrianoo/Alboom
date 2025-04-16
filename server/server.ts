2a742be33a3b9682f696e018a200dc234327fab6
julindoti <julinmestredoti@gmail.com>
2025-04-16 20:35:37 -0300

    Dockerfile + POST

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 
+
 import userRoutes from './routes/user';
 
 const app= express();
 const PORT = 3333;
 
 app.use(express.json());
-app.use('/api/users', userRoutes);
+app.use('/api', userRoutes);
 
 app.listen(PORT, ()=>{
 
-    console.log("No ar e rodando")
+    console.log(`No ar e rodando na porta ${PORT}`)
 
 })
 
